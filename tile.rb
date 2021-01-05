52e5310231a369428d5df0b2f42f9d2ae76b9632
TheClawMonster <negrilaclaudia@gmail.com>
2021-01-05 14:33:12 +0100

    base blank tile

--- tile.rb
+++ tile.rb
@@ -1,3 +1,28 @@
 class Tile
 
+  attr_acessor :revealed, :bombed, :flagged
+
+  def initialize(revealed = false, bombed = false, flagged = false)
+    @revealed = revealed
+    @bombed = bombed
+    @flagged = flagged
+    @fringe = nil
+  end
+
+  def reveal
+    @revealed = true
+  end
+
+  def flag
+    if flagged
+      flagged = false
+    else
+      flagged = true
+    end
+  end
+
+  def finge_change
+    
+  end
+
 end
